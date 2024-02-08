9cd9b863af81b464acdbff99a2dff677cb1be637
entrezunfredici <superfred2468@gmail.com>
2024-02-08 23:10:25 +0100

    failed bug fix attempt

---
+++ dependencies-install-script.py
@@ -0,0 +1,25 @@
+import os
+print("========================================================================")
+print("bienvennue dans le script d'installation de dépendances")
+command="npm install express"
+print("execution de la commande ⇒  "+command)
+os.system(command)
+command="npm install moment --save"
+print("execution de la commande ⇒  "+command)
+os.system(command)
+command="npm install jest"
+print("execution de la commande ⇒  "+command)
+os.system(command)
+command="npm install nodemon --save-dev"
+print("execution de la commande ⇒  "+command)
+os.system(command)
+command="npm install nodemon --save-dev"
+print("execution de la commande ⇒  "+command)
+os.system(command)
+command="npm install sequelize"
+print("execution de la commande ⇒  "+command)
+os.system(command)
+command="npm install sqlite3"
+print("execution de la commande ⇒  "+command)
+os.system(command)
+print("========================================================================")
