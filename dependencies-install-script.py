d0d53cd26f3bb8fb80b1a0ecb83c24898f54af0d
entrezunfredici <superfred2468@gmail.com>
2024-02-09 22:46:24 +0100

    [FIX] bugs

---
+++ dependencies-install-script.py
@@ -0,0 +1,15 @@
+import os
+print("========================================================================")
+print("bienvennue dans le script d'installation de dépendances")
+commands= {
+        "npm install express",
+        "npm install nodemon --save-dev",
+        "npm install sequelize",
+        "npm install mysql2",
+        "npm install sqlite3",
+        "npm install --save-dev jest supertest"
+        }
+for command in commands:
+    print("execution de la commande ⇒  "+command)
+    os.system(command)
+print("========================================================================")
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const port = 8000;
-const app = require('./app');
 const db = require('./models/index');
+const app = require('./app');
 // Start the server on the given port
-db.instance.sync({force: true}).then(async () => {
-    console.log('Database connected and synchronized');
+db.instance.sync({force: true}).then(async () =>{
     app.listen(port, () => {
         console.log('Server is running on port', port)
     })
-}).catch((e)=>{
-    console.error(e);
+}).catch((e) => {
+    console.rerror(e);
 })
