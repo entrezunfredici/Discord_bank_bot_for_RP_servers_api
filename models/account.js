8bed7e89ae01562349b05e2d2b5ce651a6e514bd
entrezunfredici <superfred2468@gmail.com>
2024-02-09 23:30:12 +0100

    [IMP] account model

---
+++ models/account.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (instance) => {
+    return instance.define('account', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        beneficiaryId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        identifier: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allownull: false
+        },
+        balance: {
+            type: DataTypes.INTEGER,
+            allownull: false
+        },
+    })
+}
