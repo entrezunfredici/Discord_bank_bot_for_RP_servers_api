7ea4b376230bafedc4fc87a82d709947f0aa8d0f
entrezunfredici <superfred2468@gmail.com>
2024-02-18 09:37:15 +0100

    [IMP] password verification

--- services/account.js
+++ services/account.js
@@ -26,8 +26,24 @@ exports.addAccount = async (beneficiaryId, password, balance) => {
         if(!balance){
             balance=0
         }
-        if(password.length <= 10){
-            throw new BadRequest("password must be at least 10 characters long")
+        passwordScore = 0;
+        if (password.length > 10) {
+            passwordScore+=1;
+        }
+        if(/\d/.test(password)){
+            passwordScore += 1;
+        }
+        if(/[a-z]/.test(password)){
+            passwordScore += 1;
+        }
+        if(/[A-Z]/.test(password)){
+            passwordScore += 1;
+        }
+        if(/[^a-zA-Z0-9]/.test(password)){
+            passwordScore += 1;
+        }
+        if(passwordScore < 5){
+            throw new BadRequest("password must be at least 10 characters long, one Maj, one min, one number and one special character")
         }
         return bcrypt.hash(password, 10).then((hash) => {
             return account.create({beneficiaryId, password: hash, balance})
